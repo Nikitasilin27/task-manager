4a8433da90c05f11985bb5719bb03a7c8e5eded2
Nikita Silin <142921230+Nikitasilin27@users.noreply.github.com>
2025-03-11 11:38:48 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,15 +2,16 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-if DATABASE_URL is None:
+# Получаем DATABASE_URL из переменной окружения
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set in environment variables")
-print(f"Using DATABASE_URL: {DATABASE_URL}")  # Добавляем для отладки
-if DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_async_engine(DATABASE_URL, echo=True)  # echo=True для отладки SQL-запросов
+
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
 
 async def get_db():
     async with async_session() as session:
