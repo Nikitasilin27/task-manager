95a407666b1d36f0e0199a038b43d889d2c7c052
Nikita Silin <142921230+Nikitasilin27@users.noreply.github.com>
2025-03-11 08:33:16 +0300

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -5,7 +5,15 @@ from crud import create_task
 async def test_db():
     try:
         async with async_session() as session:
-            task = await create_task(session, title="Тестовая задача", description="Проверяем работу базы")
+            task = await create_task(
+                session,
+                title="Тестовая задача",
+                description="Проверяем работу базы",
+                deadline="2025-03-11T12:00:00",
+                priority="Medium",
+                reminder=False,
+                completed=False
+            )
             print(f"✅ Создана задача: ID={task.id}, Название={task.title}")
     except Exception as e:
         print(f"Ошибка: {e}")
