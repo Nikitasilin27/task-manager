32645705036f8f4fc1f8a278bf21511fd0900fed
Nikita Silin <142921230+Nikitasilin27@users.noreply.github.com>
2025-03-11 08:36:42 +0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -5,6 +5,7 @@ from models import Base
 async def init_db():
     async with engine.begin() as conn:
         print("Модели, которые SQLAlchemy видит:", Base.metadata.tables.keys())
+        await conn.run_sync(Base.metadata.drop_all)  # Удаляем таблицы для чистоты (опционально)
         await conn.run_sync(Base.metadata.create_all)
         print("Таблицы успешно созданы.")
 
