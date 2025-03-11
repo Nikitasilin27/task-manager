16c6800edbd66e74b5aba178d1c9e8902bff64f1
Nikita Silin <142921230+Nikitasilin27@users.noreply.github.com>
2025-03-11 08:34:22 +0300

    Update database.py

--- database.py
+++ database.py
@@ -5,6 +5,8 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL is not set in environment variables")
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
