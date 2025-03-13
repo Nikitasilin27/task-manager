a91a258ea9e077a757abcc1fd28fd0162cc7c9fa
Nikita Silin <142921230+Nikitasilin27@users.noreply.github.com>
2025-03-13 08:50:27 +0300

    Update crud.py

--- crud.py
+++ crud.py
@@ -4,12 +4,10 @@ from sqlalchemy import and_
 from models import Task
 from datetime import datetime, timedelta
 
-async def get_tasks(db: AsyncSession, date: str = None):
-    query = select(Task)
+async def get_tasks(db: AsyncSession, user_id: int, date: str = None):
+    query = select(Task).where(Task.user_id == user_id)
     if date:
-        # Преобразуем переданную строку в объект date
         filter_date = datetime.strptime(date, "%Y-%m-%d").date()
-        # Создаём нижнюю и верхнюю границу как объекты datetime
         lower_bound = datetime.combine(filter_date, datetime.min.time())
         upper_bound = lower_bound + timedelta(days=1)
         query = query.where(
@@ -21,7 +19,7 @@ async def get_tasks(db: AsyncSession, date: str = None):
     result = await db.execute(query)
     return result.scalars().all()
 
-async def create_task(db: AsyncSession, title: str, description: str = None, deadline: str = None, priority: str = "Medium", reminder: bool = False, completed: bool = False):
+async def create_task(db: AsyncSession, user_id: int, title: str, description: str = None, deadline: str = None, priority: str = "Medium", reminder: bool = False, completed: bool = False):
     if priority not in ["High", "Medium", "Low"]:
         raise ValueError("Priority must be 'High', 'Medium', or 'Low'")
     deadline_dt = datetime.fromisoformat(deadline) if deadline else None
@@ -31,27 +29,31 @@ async def create_task(db: AsyncSession, title: str, description: str = None, dea
         deadline=deadline_dt,
         priority=priority,
         reminder=reminder,
-        completed=completed
+        completed=completed,
+        user_id=user_id  # Добавляем user_id
     )
     db.add(db_task)
     await db.commit()
     await db.refresh(db_task)
     return db_task
 
-async def delete_task(db: AsyncSession, task_id: int):
+async def delete_task(db: AsyncSession, user_id: int, task_id: int):
     task = await db.get(Task, task_id)
-    if not task:
+    if not task or task.user_id != user_id:
         return None
     await db.delete(task)
     await db.commit()
     return task
 
 
-async def update_task(db: AsyncSession, task_id: int, completed: bool):
+async def update_task(db: AsyncSession, user_id: int, task_id: int, completed: bool):
     task = await db.get(Task, task_id)
-    if not task:
+    if not task or task.user_id != user_id:
         return None
     task.completed = completed
+    await db.commit()
+    await db.refresh(task)
+    return task
     await db.commit()
     await db.refresh(task)
     return task
