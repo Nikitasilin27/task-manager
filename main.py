1ba3c64cbfd32fff0ac051315a73264462f2093b
Nikita Silin <142921230+Nikitasilin27@users.noreply.github.com>
2025-03-14 11:48:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,6 @@ class TaskUpdate(BaseModel):
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     yield
 
